a93465bf475a0d00c659fc68d74bb4acef97bc5d
ThiagooAndrade <andradethiago1609@gmail.com>
2025-05-23 21:16:14 -0300

    ..

--- FirebaseConfig.ts
+++ FirebaseConfig.ts
@@ -6,14 +6,7 @@ import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBZVsNj8Ayn3A3CQLA4h1VJq1ajgChAzyA",
-  authDomain: "proativo-c02f2.firebaseapp.com",
-  projectId: "proativo-c02f2",
-  storageBucket: "proativo-c02f2.firebasestorage.app",
-  messagingSenderId: "285937887978",
-  appId: "1:285937887978:web:82685111c724b4e715c593"
-};
+// coloca aqui
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
